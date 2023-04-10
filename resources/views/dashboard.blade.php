b94dd957a94d231581a5acc82e31677665f7c9b8
KurniawatiAgustina <kurniawatiagustina450@gmail.com>
2023-04-10 16:14:15 +0700

    CRUD Pegawai - Membuat tampilan dashboard dan sidebar untuk data pegawai dan film

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.template')
+
+@section('content')
+<section class="content">
+
+    <!--Default box-->
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Data Pegawai</h3>
+
+            {{-- <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widge="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+                <button type="button" class="btn btn-tool" data-card-widge="remove" title="Remove">
+                    <i class="fas fa-times"></i>
+                </button>
+            </div> --}}
+        </div>
+        <div class="card-body">
+              
+    </div>
+    <!-- /.card -->
+
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -18,10 +19,10 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
