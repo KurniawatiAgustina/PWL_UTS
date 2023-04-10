2818829b7a57a74dbe469c4410e01ebd2fe2a7d1
KurniawatiAgustina <kurniawatiagustina450@gmail.com>
2023-04-10 14:46:05 +0700

    CRUD Pegawai - Membuat Authentication pada Pegawai

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
