2c78a0396b7a4119326b0fa026147dfc20a4d8f6
KurniawatiAgustina <kurniawatiagustina450@gmail.com>
2023-04-10 14:43:47 +0700

    CRUD Pegawai - Konfirgurasi PegawaiModel

--- app/Models/PegawaiModel.php
+++ app/Models/PegawaiModel.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class PegawaiModel extends Model
 {
+    protected $table = "pegawai";
+    protected $fillable = [
+        'nama', 
+        'id_pegawai', 
+        'JK', 
+        'jabatan',
+        'no_telp',
+        'tempat_lahir', 
+        'tanggal_lahir', 
+        'alamat'
+    ];
     use HasFactory;
 }
